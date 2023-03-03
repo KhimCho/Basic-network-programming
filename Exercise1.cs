289db10b9d4ac72f076014d8cbb36d4262acd701
KhimCho <95128995+KhimCho@users.noreply.github.com>
2023-03-03 08:16:49 +0700

    Update

--- Exercise1.cs
+++ Exercise1.cs
@@ -1,4 +1,4 @@
-﻿Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+﻿// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 
 using System;
---
+++ Exercise2.cs
@@ -0,0 +1,15 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+using System;
+
+public class kata
+{
+  public static string BooleanToString(bool b)
+  {
+    if (b == true)
+        return "True";
+    else return "False";
+  }
+}
