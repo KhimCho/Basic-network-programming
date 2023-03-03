8bb5c4abd6b17ea3e0244fcb238f3066a0b935f0
KhimCho <95128995+KhimCho@users.noreply.github.com>
2023-03-03 08:06:35 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    if (word == true)
+    return "Yes";
+    else return "No";
+  }
+}
